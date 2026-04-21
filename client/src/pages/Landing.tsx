e326ad29659cdb213c9ec07f5f830476aa8225b5
dino0908 <dinokhaw@yahoo.com.sg>
2026-04-21 17:58:41 +0800

    feat: install MUI

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,6 +1,6 @@
 function Landing() {
     return (
-        <h1>landing page</h1>
+        <h1 className="flex items-center justify-center h-screen text-red-500">Hi</h1>
     )
 }
 export default Landing
