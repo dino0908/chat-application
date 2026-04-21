79a4b5db663f541ff96a8ae329ac59131ccf9ad5
dino0908 <dinokhaw@yahoo.com.sg>
2026-04-21 20:00:46 +0800

    feat: connect nodejs to postgres

--- server/src/controller/authController.js
+++ server/src/controller/authController.js
@@ -1,14 +1,36 @@
+import pool from "../config/db.js";
+
 export const login = (req, res) => {
-//   const { email, password } = req.body;
-//   console.log("Login Attempt:", { email, password });
-  
+  //   const { email, password } = req.body;
+  //   console.log("Login Attempt:", { email, password });
+
   // For now, just send a success response
   res.status(200).json({ message: "Login endpoint hit successfully" });
 };
 
 export const register = (req, res) => {
-//   const { username, email, password } = req.body;
-//   console.log("Register Attempt:", { username, email, password });
-  
+  //   const { username, email, password } = req.body;
+  //   console.log("Register Attempt:", { username, email, password });
+
   res.status(201).json({ message: "Register endpoint hit successfully" });
+};
+
+export const getUsers = async (req, res) => {
+  try {
+    const result = await pool.query(
+      "SELECT id, username, email, created_at FROM users ORDER BY created_at DESC",
+    );
+
+    res.status(200).json({
+      success: true,
+      count: result.rowCount,
+      data: result.rows,
+    });
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
 };
--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { login, register } from '../controller/authController.js';
+import { login, register, getUsers } from '../controller/authController.js';
 
 const router = express.Router();
 
 router.post('/login', login)
 router.post('/register', register)
+router.get('/users', getUsers)
 
 
 
